f28247e801c72eb695dadd329d24bc2b8081037a
mauricio gonzales <mauri9825@gmail.com>
2022-09-06 17:56:06 -0300

    Agregadas rutas

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import ListaPlatos from './components/ListaPlatos';
 import EditarPlato from './components/EditarPlato';
+import AgregarPlato from './components/AgregarPlato';
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <h1>Fusiones Gestion Comercial</h1>
-      <ListaPlatos/>
-      <EditarPlato/>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<ListaPlatos/>}></Route>
+        <Route path='/agregarplato' element={<AgregarPlato/>}></Route>
+        <Route path='/editarplato' element={<EditarPlato/>}></Route>
+      </Routes>
+    </BrowserRouter>
+
     </div>
   );
 }
---
+++ cliente/src/components/AgregarPlato.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function AgregarPlato(){
+    return(
+        <div>
+            <h2>Agregar Plato</h2>
+        </div>
+    )
+}
+
+export default AgregarPlato;
