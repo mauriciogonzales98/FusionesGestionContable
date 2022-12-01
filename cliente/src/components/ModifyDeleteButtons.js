f83f391891cd366c06b0983b224cce97bdc60b62
mauricio gonzales <mauri9825@gmail.com>
2022-12-01 17:50:16 -0300

    Iconos agregados

--- cliente/src/components/ModifyDeleteButtons.js
+++ cliente/src/components/ModifyDeleteButtons.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-regular-svg-icons"
+import { faTrash } from "@fortawesome/free-solid-svg-icons"
 
 function ModifyDeleteButtons(){
 
     return(
-        <td>
-            <button>+</button>
-            <button>-</button>
+        <td className="space-x-3">
+            <button> <FontAwesomeIcon icon={ faPenToSquare } /></button>
+            <button> <FontAwesomeIcon icon={ faTrash } /></button>
+            {/* <button>+</button>
+            <button>-</button> */}
         </td>
     )
 }
